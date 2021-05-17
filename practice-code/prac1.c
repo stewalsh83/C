644bf243012d0fc75c1affd9887032e9f8b3b147
walss243 <stephen.walsh243@mail.dcu.ie>
2021-05-18 00:03:40 +0100

    adding file

---
+++ practice-code/prac1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() // main method (container where we put code)
+{
+    // inside the method
+    printf("    /|\n");
+    printf("   / |\n");
+    printf("  /  |\n");
+    printf(" /   |\n");
+    return 0;
+}
