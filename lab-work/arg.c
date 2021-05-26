34cf5e83a33e2b2b85b5b04bf2c329c1a45a7b09
walss243 <stephen.walsh243@mail.dcu.ie>
2021-05-26 15:17:03 +0100

    adding lab work

---
+++ lab-work/arg.c
@@ -0,0 +1,19 @@
+#include <stdio.h> 
+
+int main( int argc, char *argv [] )
+{
+    printf(" \n Name of my Program %s \t", argv[0]);
+
+    if( argc == 2 )
+    {
+        printf("\n Value given by user is: %s \t", argv[1]);
+    }
+    else if( argc > 2 )
+    {
+        printf("\n Many values given by users.\n");
+    }
+    else
+    {
+        printf(" \n Single value expected.\n");
+    }
+}
---
+++ lab-work/hailstones.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+/* main fuction */
+int main( int argc, char const *argv[] )
+{
+	int num;
+	num = atoi(argv[1]); /* assigning a variable to argv[1]*/ 
+
+	if(argc == 2){ /* if pointer is = to 2 then it will run the while loop */
+
+		while( num != 1 ){ /* loop until 1 is reached */
+
+			if( num % 2 == 0 ){     /* hailstone formula */
+				num = ( num / 2 );
+			} else {
+				num = ( (num * 3) + 1 );
+			}
+			printf( "%d\n", num ); 
+		}
+			
+	}
+
+	return 0;
+
+}
