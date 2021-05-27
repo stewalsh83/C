83973f17ee8f1a52a11cbd0e1460d1129524baee
walss243 <stephen.walsh243@mail.dcu.ie>
2021-05-27 14:39:00 +0100

    testing Makefile

--- learn-c-thw/prac1/ex1.c
+++ learn-c-thw/prac1/ex1.c
@@ -3,9 +3,11 @@
 /* This is a comment. */
 int main(int argc, char *argv[])
 {
+    // assign 100 to the distance integer
     int distance = 100;
 
     // This is also a comment.
-    printf("You are %d distance away\n", distance);
+    printf("You are %d miles away\n", distance);
+    
     return 0;
 }
