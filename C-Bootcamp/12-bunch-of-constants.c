d0d5fe618da552991f2ef80b8b5de0be2e2e3c65
walss243 <stephen.walsh243@mail.dcu.ie>
2021-06-15 18:36:49 +0100

    adding challenges

---
+++ C-Bootcamp/12-bunch-of-constants.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * === Bunch Of Constants === 
+ *
+ * Define the following numeric constants:
+ *  - moonLanding: 1969
+ *  - speedOfLight: 299792458
+ *  - pi: 3.142
+ *  - hexaDead: 0xDEAD
+ *  - hexaSecret: 51966
+ * 
+ * Print with following formats:
+ *  - moonLanding:
+ *      - integer, 10 characters width, padded with spaces
+ *      - integer, 10 characters width, padded with zeroes
+ *  - speedOfLight:
+ *      - floating point without digits after decimal point
+ *      - scientific notation with 3 digit of precision
+ *  - pi:
+ *      - floating point with 2 digits of precision
+ *      - scientific notation with 1 digit of precision, sign forced
+ *  - hexaDead:
+ *      - unsigned hexadecimal integer, uppercase, leading 0x
+ *      - unsigned integer, 6 characters width
+ *  - hexaSecret:
+ *      - unsigned hexadecimal integer, lowercase, no leading 0x
+ * 
+ */
+
+int main(void)
+{
+    printf("Bunch of constants\n");
+
+    const int moonLanding = 1969;
+    const double speedOfLight = 299792458.;
+    const double pi = 3.142;
+    const unsigned hexaDead = 0xDEADU;
+    const unsigned hexaSecret = 51966U;
+
+    printf("Moon Landing:\n %10d\n %010d\n\n", moonLanding, moonLanding);
+    printf("Speed of Light:\n %.0f\n %.3e\n\n",speedOfLight, speedOfLight);
+    printf("Pi:\n %.2f\n %+.1e\n\n",pi, pi); // + force the sign
+    printf("Hexa Dead:\n 0x%X\n %6u\n\n",hexaDead, hexaDead);
+    printf("Hexa Secret:\n %x\n\n", hexaSecret);
+
+    printf("\n\n === C === \n\n");
+    return 0;
+}
+
+// 1. define constants
