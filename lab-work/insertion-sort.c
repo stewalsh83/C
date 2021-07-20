95d2a6b5de2a6c4552931e56ea22d60b849dfac6
walss243 <stephen.walsh243@mail.dcu.ie>
2021-07-20 01:08:54 +0100

    adding c files

--- lab-work/insertion-sort.c
+++ lab-work/insertion-sort.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
-// selection sort from command line
+// insertion sort from command line
 int main(int argc, char *argv[])
 {
-    int i, j, num, temp, pos;
+    int i, num, pos, val;
 	int size = 0;
 
 	for(num = 1; num < argc; num++){
@@ -13,30 +13,26 @@ int main(int argc, char *argv[])
 	int array[100]; //int array[size];
 	array[size];
 	for(i = 0; i <= size; i++)
-		array[i] = atoi(argv[i + 1]);
+	    array[i] = atoi(argv[i + 1]);
 
-	i = 0;
+	i = 1;
 	while(i < size)
 	{   
+        val = array[i];
         pos = i;
-        j = i + 1;
-        while(j < size)
+        while(0 < pos && val < array[pos - 1])
         {
-            if(array[j] < array[pos])
-                pos = j;
-            j++;
+            array[pos] = array[pos - 1];
+            pos -= 1;
         }
-		temp = array[pos];
-		array[pos] = array[i];
-		array[i] = temp;
-		i++;
+        array[pos] = val;
+        i++;
 	}
-
-    printf("Selection Sort\n");
+    printf("Insertion Sort\n");
 	printf("Sorted array: \n");
 
 	for(i = 0; i < size; i++)
-		printf("%d ", array[i]);
+	    printf("%d ", array[i]);
 
 	printf("\n\n");
 	return 0;
