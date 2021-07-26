c9857603dde5566929d85606212cf3a35ac9e0fd
walss243 <stephen.walsh243@mail.dcu.ie>
2021-07-26 20:06:10 +0100

    format

--- lab-work/insertion-sort.c
+++ lab-work/insertion-sort.c
@@ -4,36 +4,37 @@
 int main(int argc, char *argv[])
 {
     int i, num, pos, val;
-	int size = 0;
+    int size = 0;
 
-	for(num = 1; num < argc; num++){
-		size++;
-	}
+    for (num = 1; num < argc; num++)
+    {
+        size++;
+    }
 
-	int array[100]; //int array[size];
-	array[size];
-	for(i = 0; i <= size; i++)
-	    array[i] = atoi(argv[i + 1]);
+    int array[100]; //int array[size];
+    array[size];
+    for (i = 0; i <= size; i++)
+        array[i] = atoi(argv[i + 1]);
 
-	i = 1;
-	while(i < size)
-	{   
+    i = 1;
+    while (i < size)
+    {
         val = array[i];
         pos = i;
-        while(0 < pos && val < array[pos - 1])
+        while (0 < pos && val < array[pos - 1])
         {
             array[pos] = array[pos - 1];
             pos -= 1;
         }
         array[pos] = val;
         i++;
-	}
+    }
     printf("Insertion Sort\n");
-	printf("Sorted array: \n");
+    printf("Sorted array: \n");
 
-	for(i = 0; i < size; i++)
-	    printf("%d ", array[i]);
+    for (i = 0; i < size; i++)
+        printf("%d ", array[i]);
 
-	printf("\n\n");
-	return 0;
+    printf("\n\n");
+    return 0;
 }
--- lab-work/selection-sort.c
+++ lab-work/selection-sort.c
@@ -3,39 +3,40 @@
 // selection sort from command line
 int main(int argc, char *argv[])
 {
-    int i, j, num, temp, pos;
+	int i, j, num, temp, pos;
 	int size = 0;
 
-	for(num = 1; num < argc; num++){
+	for (num = 1; num < argc; num++)
+	{
 		size++;
 	}
 
 	int array[100]; //int array[size];
 	array[size];
-	for(i = 0; i <= size; i++)
+	for (i = 0; i <= size; i++)
 		array[i] = atoi(argv[i + 1]);
 
 	i = 0;
-	while(i < size)
-	{   
-        pos = i;
-        j = i + 1;
-        while(j < size)
-        {
-            if(array[j] < array[pos])
-                pos = j;
-            j++;
-        }
+	while (i < size)
+	{
+		pos = i;
+		j = i + 1;
+		while (j < size)
+		{
+			if (array[j] < array[pos])
+				pos = j;
+			j++;
+		}
 		temp = array[pos];
 		array[pos] = array[i];
 		array[i] = temp;
 		i++;
 	}
 
-    printf("Selection Sort\n");
+	printf("Selection Sort\n");
 	printf("Sorted array: \n");
 
-	for(i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 		printf("%d ", array[i]);
 
 	printf("\n\n");
